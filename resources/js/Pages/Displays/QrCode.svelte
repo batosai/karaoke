f5f351312a3e47d0b7000e663aac4b2f97cfbbe6
jeremy <jeremy@chaufourier.fr>
2022-05-07 15:39:30 +0200

    Refacto qrcode display

--- resources/js/Pages/Displays/QrCode.svelte
+++ resources/js/Pages/Displays/QrCode.svelte
@@ -3,7 +3,16 @@
    import { room } from '../../stores'
 </script>
 
-<div class="fixed top-10 right-10 bg-white rounded-box p-5 z-50">
-  <QrCode value={$room.fullUri} size="150" />
-  <p>{ $room.pin }</p>
+<div class="fixed top-10 right-10 z-50">
+  <div class="bg-white rounded-box p-5 z-50">
+    <QrCode value={$room.fullUri} size="150" />
+  </div>
+  <div class="grid mt-2 place-items-center">
+    <div class="flex gap-2 text-xl text-white">
+      <div class="flex items-center justify-center bg-blue-500 rounded-box w-8 h-8">{ $room.pin[0] }</div>
+      <div class="flex items-center justify-center bg-blue-500 rounded-box w-8 h-8">{ $room.pin[1] }</div>
+      <div class="flex items-center justify-center bg-blue-500 rounded-box w-8 h-8">{ $room.pin[2] }</div>
+      <div class="flex items-center justify-center bg-blue-500 rounded-box w-8 h-8">{ $room.pin[3] }</div>
+    </div>
+  </div>
 </div>
