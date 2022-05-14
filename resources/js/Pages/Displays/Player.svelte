e4a730d1c154455997a9793f7f601597c672caf6
jeremy <jeremy@chaufourier.fr>
2022-05-14 10:20:32 +0200

    Update video position

--- resources/js/Pages/Displays/Player.svelte
+++ resources/js/Pages/Displays/Player.svelte
@@ -13,31 +13,39 @@
   export let state = { ...initialeState }
 </script>
 
-<Player
-  controls
-  autoplay
-  paused="true"
-  bind:this={player}
-  class="{ state.played ? '' : 'hidden' }"
->
-  <Video>
-    <source data-src="{ state.trackUrl }"  type="video/mp4" />
-  </Video>
+<div class="hero min-h-screen block">
+  <Player
+    controls
+    autoplay
+    paused="true"
+    bind:this={player}
+    class="{ state.played ? '' : 'hidden' }"
+  >
+    <Video>
+      <source data-src="{ state.trackUrl }"  type="video/mp4" />
+    </Video>
 
-  <Ui>
-    <ClickToPlay />
+    <Ui>
+      <ClickToPlay />
 
-    <footer class="footer p-4 absolute bottom-0 z-[100]">
-      <div class="avatar-group -space-x-6">
-        {#each $players as p}
-          <div class="avatar">
-            <div class="w-12">
-              <img src="{ p.avatar }" alt="" />
-              <audio controls="true" autoplay="true" id={p.socketId} />
+      <footer class="footer p-4 absolute bottom-0 z-[100]">
+        <div class="avatar-group -space-x-6">
+          {#each $players as p}
+            <div class="avatar">
+              <div class="w-12">
+                <img src="{ p.avatar }" alt="" />
+                <audio controls="true" autoplay="true" id={p.socketId} />
+              </div>
             </div>
-          </div>
-        {/each}
-      </div>
-    </footer>
-  </Ui>
-</Player>
+          {/each}
+        </div>
+      </footer>
+    </Ui>
+  </Player>
+
+  <div class="hero-content text-center text-neutral-content">
+    <div class="max-w-md">
+
+    </div>
+  </div>
+</div>
