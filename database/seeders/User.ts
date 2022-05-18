993af6b07d27c9b421229aed5caa22567df9835b
jeremy <jeremy@chaufourier.fr>
2022-05-18 22:35:13 +0200

    Update User.ts

--- database/seeders/User.ts
+++ database/seeders/User.ts
@@ -2,7 +2,7 @@ import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
 import User from 'App/Models/User'
 
 export default class UserSeeder extends BaseSeeder {
-  public static developmentOnly = true
+  // public static developmentOnly = true
 
   public async run() {
     await User.createMany([
