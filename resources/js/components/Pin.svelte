9f19e76b08905261638ae8f7486f4a5c3658cd9b
jeremy <jeremy@chaufourier.fr>
2022-05-22 20:18:44 +0200

    Update Pin.svelte

--- resources/js/components/Pin.svelte
+++ resources/js/components/Pin.svelte
@@ -6,19 +6,41 @@
 
   let disabled = false
 
+  const KEYBOARD = {
+    BACKSPACE: 8,
+    DELETE: 46,
+    ANDROID_BACKSPACE: 229,
+  }
+
   $: if (errors !== null) {
     disabled = false
   }
 
-  const changeHandler = function (e) {
+  const isKeyDelete = key => {
+    let result =
+      key === KEYBOARD.BACKSPACE ||
+      key === KEYBOARD.DELETE ||
+      key === KEYBOARD.ANDROID_BACKSPACE
+    return result
+  }
+
+  const init = el => {
+    el.focus()
+  }
+
+  const changeHandler =  e => {
     if(pin.length === size) {
       disabled = true
+      e.target.blur()
       submit(pin)
     }
   }
 
-  function init(el){
-    el.focus()
+  // For android
+  const maxlength = e => {
+    if(pin.length === size && !isKeyDelete(e.keyCode)) {
+      e.preventDefault()
+    }
   }
 </script>
 
@@ -31,6 +53,7 @@
       placeholder="xxxx"
       maxlength="4"
       on:keyup={changeHandler}
+      on:keydown={maxlength}
       bind:value={pin}
       disabled={disabled}
       class="input input-bordered w-48 flex items-center text-center text-4xl uppercase tracking-widest bg-gray-200" />
