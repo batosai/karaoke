1d616299100f7db86e7fc3bee5325075068e0c19
jeremy <jeremy@chaufourier.fr>
2022-06-01 00:13:50 +0200

    test quality 136

--- app/Controllers/Http/MediasController.ts
+++ app/Controllers/Http/MediasController.ts
@@ -3,6 +3,7 @@ import Env from '@ioc:Adonis/Core/Env'
 import Track from 'App/Models/Track'
 import ytdl from 'ytdl-core'
 import ffmpeg from 'fluent-ffmpeg'
+import fs from 'fs'
 
 export default class MediasController {
   public async show({ response, request }: HttpContextContract) {
@@ -11,28 +12,46 @@ export default class MediasController {
     ffmpeg.setFfmpegPath(Env.get('FFMPEG_PATH'))
     // ffmpeg.setFfprobePath(Env.get('FFPROBE_PATH'))
 
-    response.header('Content-Type', `video/mp4`)
+    // response.header('Content-Type', `video/mp4`)
 
-    const command = ffmpeg({ source: ytdl(track.url, { quality: '18' }) })
-      // .videoCodec('libx264')
-      // .audioCodec('libmp3lame')
-      .on('error', function(err) {
-        console.log('An error occurred: ' + err.message);
-      })
-      // .ffprobe(0, function(err, data) {
-      //   console.log('file1 metadata:');
-      //   console.dir(data);
-      // })
-      .outputOptions(['-frag_duration 100','-movflags frag_keyframe+empty_moov+faststart'])
-      .toFormat('mp4')
+    // const command = ffmpeg({ source: ytdl(track.url, { quality: '136' }) })
+    // // const command = ffmpeg().input(ytdl(track.url, { quality: '136' }))
 
-      return response.stream(command.pipe())
+    //   .on('error', function(err) {
+    //     console.log('An error occurred: ' + err.message);
+    //   })
+    //   // .ffprobe(0, function(err, data) {
+    //   //   console.log('file1 metadata:');
+    //   //   console.dir(data);
+    //   // })
 
-//     const meta = await ytdl.getInfo(track.url)
+    //   .outputOptions(['-frag_duration 100','-movflags frag_keyframe+empty_moov+faststart'])
+    //   .toFormat('mp4')
+
+    //   .mergeAdd(ytdl(track.url, { quality: 'highestaudio' }))
+
+    //   // .videoCodec('libx264')
+    //   // .audioCodec('libmp3lame')
+    //   // .toFormat('webm')
+    //   // .videoCodec('libvpx-vp9')
+    //   // .audioCodec('libmp3lame')
+
+    //   return response.stream(command.pipe())
+
+    // const meta = await ytdl.getInfo(track.url)
+
+    // const title = meta.videoDetails.title.normalize('NFD').replace(/[\u0300-\u036f]/g, '')
+
+    // Set content headers to download stream as a file
+    // response.header('content-disposition', `attachment; filename="${encodeURIComponent(title)}.mp4"`)
+    // return response.stream(ytdl(track.url))
+
+
 // console.log(meta)
-//     // return response.stream(ytdl(track.url, { quality: '133' }))
-//     return response.stream(ytdl(track.url, {
-//       filter: format => format.container === 'mp4',
-//     }))
+    return response.stream(ytdl(track.url, { quality: '136' }))
+    // return response.stream(ytdl(track.url, { quality: 'highestaudio' }))
+    // return response.stream(ytdl(track.url, {
+    //   filter: format => format.container === 'mp4',
+    // }))
   }
 }
