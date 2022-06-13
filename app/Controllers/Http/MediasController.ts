692ecd17be4c170571382276b2afac028da9ad0a
jeremy <jeremy@chaufourier.fr>
2022-06-13 22:31:37 +0200

    Update MediasController.ts

--- app/Controllers/Http/MediasController.ts
+++ app/Controllers/Http/MediasController.ts
@@ -5,6 +5,12 @@ import ytdl from 'ytdl-core'
 export default class MediasController {
   public async show({ response, request }: HttpContextContract) {
     const track = await Track.findOrFail(request.param('id'))
+    const info = await ytdl.getInfo(track.url)
+    const format = ytdl.chooseFormat(info.formats, { quality: '18' })
+
+    response.header('Content-Type', format.mimeType!)
+    response.header('Content-Length', format.contentLength)
+    response.header('Connection', `keep-alive`)
 
     return response.stream(ytdl(track.url, { quality: '18' }))
   }
